e1ae714e65f3d8101ca261c1a8c31a37101610ae
Matheus De Padua Domingos <you@example.com>
2019-03-19 08:27:38 -0300

    Testando botoes tela de login

--- app/src/main/java/com/treinamento/mdomingos/startapp/LoginActivity.java
+++ app/src/main/java/com/treinamento/mdomingos/startapp/LoginActivity.java
@@ -2,12 +2,33 @@ package com.treinamento.mdomingos.startapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private RelativeLayout botaoLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        botaoLogin = findViewById(R.id.botao_login_id);
+
+
+        botaoLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(LoginActivity.this, "Funciona", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 }
