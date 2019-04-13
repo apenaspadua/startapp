f371d701e6935feb720a7a7b0116fc2fb3daa08b
Pádua <padua.matheus13@gmail.com>
2019-04-13 11:08:42 -0300

    tela perfil

--- app/src/main/java/com/treinamento/mdomingos/startapp/activity/investidor/EditarPerfilInvestidorActivity.java
+++ app/src/main/java/com/treinamento/mdomingos/startapp/activity/investidor/EditarPerfilInvestidorActivity.java
@@ -1,6 +1,5 @@
 package com.treinamento.mdomingos.startapp.activity.investidor;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
@@ -26,20 +25,16 @@ import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.google.android.gms.tasks.Continuation;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -77,10 +72,8 @@ public class EditarPerfilInvestidorActivity extends AppCompatActivity {
     private StorageReference storageReference;
     private FirebaseStorage storage = FirebaseStorage.getInstance();
     private FirebaseUser user;
-    private static final int CHOOSE_IMAGE = 101;
-    private Uri uriProfile;
-    private Boolean changed = false;
-
+    private String imagem;
+    private FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
 
     @Override
     protected void onResume() {
@@ -334,157 +327,65 @@ public class EditarPerfilInvestidorActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        if (resultCode != Activity.RESULT_CANCELED) {
-//            progressDialog.setMessage("Uploading Image...");
-//            progressDialog.show();
-
-            if (requestCode == CHOOSE_IMAGE) {
-                uriProfile = data.getData();
-                Glide.with(EditarPerfilInvestidorActivity.this).load(uriProfile).into(foto);
-                uploadImageToFirebaseStorage();
-                changed = true;
-
-            }
-        }
-    }
-
-    // Envia imagem para o Storage do Firebase
-    private void uploadImageToFirebaseStorage() {
-        final StorageReference profileImageRef = storageReference.child("Photos").child(firebaseAuth.getCurrentUser().getUid()).child(uriProfile.getLastPathSegment());
-        final UploadTask uploadTask = profileImageRef.putFile(uriProfile);
-
-        uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
-            @Override
-            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
-                if (!task.isSuccessful()) {
-                    throw task.getException();
+        if (requestCode == 2 && resultCode == RESULT_OK) {
+            progressBar.setVisibility(View.VISIBLE);
+            if (user != null) {
+                Uri uri = data.getData();
+                foto.setImageURI(uri);
+                StorageReference filepath = storageReference.child("foto_perfil").child(user.getUid()).child(uri.getLastPathSegment());
+                Bitmap bmp = null;
+                try {
+                    bmp = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                return profileImageRef.getDownloadUrl();
-            }
-        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
-            @Override
-            public void onComplete(@NonNull Task<Uri> task) {
-                if (task.isSuccessful()) {
-                    uriProfile = task.getResult();
-                    System.out.println(uriProfile);
+                if (bmp != null) {
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    bmp.compress(Bitmap.CompressFormat.JPEG, 25, baos);
+                    byte[] dataByte = baos.toByteArray();
 
-                    progressDialog.dismiss();
-                    Toast.makeText(EditarPerfilInvestidorActivity.this, "Uploading Finished", Toast.LENGTH_SHORT).show();
-                } else {
-                    progressDialog.dismiss();
-                    Toast.makeText(EditarPerfilInvestidorActivity.this, "Uploading failed", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-    }
-
-    //Carrega informações do usuario e confirma a verificacao de email
-    private void loadUserInformation() {
-        final FirebaseUser user = firebaseAuth.getCurrentUser();
-
-        if (user != null) {
-            if (user.getPhotoUrl() != null) {
-                Glide.with(this).load(user.getPhotoUrl().toString()).into(foto);
-            } else {
-                Toast.makeText(this, "Usuario sem foto!", Toast.LENGTH_SHORT).show();
-
-
-            }
-        }
-
-    }
-
-    // Atualiza o nome e foto de perfil do usuario
-    public void saveUserInformation(View v) {
-
-        FirebaseUser user = firebaseAuth.getCurrentUser();
-        UserProfileChangeRequest profile = null;
-        if (user != null) {
-            profile =  new UserProfileChangeRequest.Builder()
-                    .setPhotoUri(uriProfile) // Define a foto do usuario
-                    .build();
-
-            user.updateProfile(profile)
-                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    filepath.putBytes(dataByte).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                         @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if (task.isSuccessful()) {
-                                Toast.makeText(EditarPerfilInvestidorActivity.this, "Profile updated", Toast.LENGTH_SHORT).show();
-                            } else {
-                                Toast.makeText(EditarPerfilInvestidorActivity.this, "Error to update", Toast.LENGTH_SHORT).show();
-                            }
+                        public void onSuccess(final UploadTask.TaskSnapshot taskSnapshot) {
+                            taskSnapshot.getMetadata().getReference().getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    firebaseFirestore.collection("users").document(user.getUid()).update("Imagem", uri.toString()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            if (imagem != null) {
+                                                if (!imagem.contains("NEXCLUIR")) {
+                                                    StorageReference ref2 = FirebaseStorage.getInstance().getReferenceFromUrl(imagem);
+                                                    ref2.delete();
+                                                    Task<Uri> downloadUri = ref2.getDownloadUrl();
+                                                    Log.d("link", downloadUri.toString());
+
+                                                }
+                                            }
+                                        }
+                                    });
+                                    Glide.with(EditarPerfilInvestidorActivity.this).load(uri.toString()).into(foto);
+                                    imagem = uri.toString();
+                                    Toast.makeText(EditarPerfilInvestidorActivity.this, "Imagem alterada!", Toast.LENGTH_SHORT).show();
+                                    progressBar.setVisibility(View.INVISIBLE);
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(EditarPerfilInvestidorActivity.this, "Erro ao alterar imagem!", Toast.LENGTH_SHORT).show();
+                                    progressBar.setVisibility(View.INVISIBLE);
+                                }
+                            });
                         }
                     });
+
+                } else {
+                    Toast.makeText(this, "erro", Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.INVISIBLE);
+                }
+            } else {
+                progressBar.setVisibility(View.INVISIBLE);
+            }
         }
     }
 }
-
-
-
-//@Override
-//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        if(requestCode == 2 && resultCode == RESULT_OK){
-//            progressBar.setVisibility(View.VISIBLE);
-//            if(user != null){
-//                Uri uri = data.getData();
-//                foto.setImageURI(uri);
-//                StorageReference filepath = storageReference.child("foto_perfil").child(user.getUid()).child(uri.getLastPathSegment());
-//                Bitmap bmp = null;
-//                try{
-//                    bmp = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-//                } catch (IOException e){
-//                    e.printStackTrace();
-//                }
-//                if(bmp != null){
-//                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//                    bmp.compress(Bitmap.CompressFormat.JPEG, 25, baos);
-//                    byte[] dataByte = baos.toByteArray();
-//
-//                    filepath.putBytes(dataByte).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-//                        @Override
-//                        public void onSuccess(final UploadTask.TaskSnapshot taskSnapshot) {
-//                            taskSnapshot.getMetadata().getReference().getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-//
-//                                @Override
-//                                public void onSuccess(Uri uri) {
-//                                    firebaseFirestore.collection("users").document(user.getUid()).update("Imagem", uri.toString()).addOnSuccessListener(new OnSuccessListener<Void>() {
-//                                        @Override
-//                                        public void onSuccess(Void aVoid) {
-//                                            if(imagem != null){
-//                                                if (!imagem.contains("NEXCLUIR")) {
-//                                                    StorageReference ref2 = FirebaseStorage.getInstance().getReferenceFromUrl(imagem);
-//                                                    ref2.delete();
-//                                                    Task<Uri> downloadUri = ref2.getDownloadUrl();
-//                                                    Log.d("link", downloadUri.toString());
-//
-//                                                }
-//                                            }
-//                                        }
-//                                    });
-//                                    Glide.with(EditarPerfilInvestidorActivity.this).load(uri.toString()).into(foto);
-//                                    imagem = uri.toString();
-//                                    Toast.makeText(EditarPerfilInvestidorActivity.this, "Imagem alterada!", Toast.LENGTH_SHORT).show();
-//                                    progressBar.setVisibility(View.INVISIBLE);
-//                                }
-//                            }).addOnFailureListener(new OnFailureListener() {
-//                                @Override
-//                                public void onFailure(@NonNull Exception e) {
-//                                    Toast.makeText(EditarPerfilInvestidorActivity.this, "Erro ao alterar imagem!", Toast.LENGTH_SHORT).show();
-//                                    progressBar.setVisibility(View.INVISIBLE);
-//                                }
-//                            });
-//                        }
-//                    });
-//
-//                } else {
-//                    Toast.makeText(this, "erro", Toast.LENGTH_SHORT).show();
-//                    progressBar.setVisibility(View.INVISIBLE);
-//                }
-//            } else {
-//                progressBar.setVisibility(View.INVISIBLE);
-//            }
-//        }
-//    }
